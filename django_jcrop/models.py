3971d2cf6156136a53f2d5f03f4e9b1779790dd5
Krzysztof Jurewicz <krzysztof.jurewicz@laboratorium.ee>
2012-11-16 17:22:38 +0100

    Ability to set image sizes and aspect ratio.

--- django_jcrop/models.py
+++ django_jcrop/models.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.contrib.admin.widgets import AdminFileWidget
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.core.files.storage import default_storage
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 from django.template.loader import get_template
 from django.template import Context
@@ -13,14 +15,21 @@ from django.utils.html import escape, conditional_escape
 from django.utils.safestring import mark_safe
 from django import forms
 
-from PIL import Image
+import Image
 from StringIO import StringIO
 
-from simplejson import loads
+from django.utils.simplejson import loads
 
 
 class ClearableFileInput(forms.ClearableFileInput):
 
+    def __init__(self, *args, **kwargs):
+        self.aspect_ratio = kwargs.pop('aspect_ratio', None)
+        self.size = kwargs.pop('size', None)
+        if self.size and not self.aspect_ratio:
+            self.aspect_ratio = float(self.size[0]) / self.size[1]
+        super(ClearableFileInput, self).__init__(*args, **kwargs)
+            
     def value_from_datadict(self, data, files, name):
         if forms.CheckboxInput().value_from_datadict(data, files, "%s-crop" % name):
             #{"x":26,"y":40,"x2":320,"y2":150,"w":294,"h":110}
@@ -33,6 +42,8 @@ class ClearableFileInput(forms.ClearableFileInput):
             box = (cdata['x'] * xr, cdata['y'] * yr, cdata['x2'] * xr, cdata['y2'] * yr)
             box = map(int, box)
             crop = im.crop(box)
+            if self.size:
+                crop.thumbnail(self.size, Image.ANTIALIAS)
             sio = StringIO()
             crop.save(sio, im.format)
             sio.seek(0)
@@ -66,11 +77,16 @@ class JCropAdminImageWidget(ClearableFileInput):
         css = {"all": (settings.STATIC_URL + "django_jcrop/css/jquery.Jcrop.css", )}
 
     def render(self, name, value, attrs=None):
+        JCROP_IMAGE_WIDGET_DIMENSIONS = getattr(settings, "JCROP_IMAGE_WIDGET_DIMENSIONS", "320x320")
+        JCROP_IMAGE_THUMBNAIL_DIMENSIONS = getattr(settings, "JCROP_IMAGE_THUMBNAIL_DIMENSIONS", "62x62")        
+        
         substitutions = {
             'initial_text': self.initial_text,
             'input_text': self.input_text,
             'clear_template': '',
             'clear_checkbox_label': self.clear_checkbox_label,
+            'aspect_ratio': self.aspect_ratio,
+            'size': list(self.size)
         }
         template = u'%(input)s'
         substitutions['input'] = super(forms.ClearableFileInput, self).render(name, value, attrs)
@@ -83,6 +99,11 @@ class JCropAdminImageWidget(ClearableFileInput):
             substitutions['initial'] = (u'<a href="%s">%s</a>'
                                         % (escape(value.url),
                                            escape(force_unicode(value))))
+
+            widget_dimensions = [int(dim) for dim in JCROP_IMAGE_WIDGET_DIMENSIONS.split("x")]
+            scale_ratio = min(float(widget_dimensions[0])/value.width, float(widget_dimensions[1])/value.height)
+            substitutions['min_size'] = [scale_ratio*dim for dim in self.size]
+            
             if not self.is_required:
                 checkbox_name = self.clear_checkbox_name(name)
                 checkbox_id = self.clear_checkbox_id(checkbox_name)
@@ -95,8 +116,8 @@ class JCropAdminImageWidget(ClearableFileInput):
 
         t = get_template("jcrop/jcrop_image_widget.html")
         substitutions.update({
-            "JCROP_IMAGE_THUMBNAIL_DIMENSIONS": getattr(settings, "JCROP_IMAGE_THUMBNAIL_DIMENSIONS", "62x62"),
-            "JCROP_IMAGE_WIDGET_DIMENSIONS": getattr(settings, "JCROP_IMAGE_WIDGET_DIMENSIONS", "320x320"),
+            "JCROP_IMAGE_THUMBNAIL_DIMENSIONS": JCROP_IMAGE_THUMBNAIL_DIMENSIONS,
+            "JCROP_IMAGE_WIDGET_DIMENSIONS": JCROP_IMAGE_WIDGET_DIMENSIONS,
         })
         c = Context(substitutions)
         return t.render(c)
@@ -104,8 +125,12 @@ class JCropAdminImageWidget(ClearableFileInput):
 
 class JCropImageField(models.ImageField):
 
+    def __init__(self, *args, **kwargs):
+        self.aspect_ratio = kwargs.pop('aspect_ratio', None)
+        self.size = kwargs.pop('size', None)
+        super(JCropImageField, self).__init__(*args, **kwargs)
+
     def formfield(self, **kwargs):
         defaults = kwargs
-        defaults.update({'form_class': forms.ImageField, 'widget': JCropAdminImageWidget})
+        defaults.update({'form_class': forms.ImageField, 'widget': JCropAdminImageWidget(aspect_ratio=self.aspect_ratio, size=self.size)})
         return super(JCropImageField, self).formfield(**defaults)
-
