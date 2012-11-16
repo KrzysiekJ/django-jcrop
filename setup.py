c3057321f95823e0a726e731cc9e1095f0e9b4a9
Krzysztof Jurewicz <krzysztof.jurewicz@laboratorium.ee>
2012-11-16 17:25:23 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-jcrop',
+      version='0.0.1',
+      description='Django app that provides a crop field using Jcrop',
+      url='https://github.com/KrzysiekJ/django-jcrop',
+      packages=['django_jcrop'],
+    )
